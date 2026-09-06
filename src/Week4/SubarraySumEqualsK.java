50cbe829144735e2cede2a5f0d24a33dfba9517e
vinaykumarlodduuu-cpu <vinaykumarlodduuu@gmail.com>
2026-09-06 14:51:52 +0530

    Add Week4 Category C assignment solutions

---
+++ src/Week4/SubarraySumEqualsK.java
@@ -0,0 +1,42 @@
+package Week4;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            currentSum += num;
+
+            int requiredSum = currentSum - k;
+
+            if (map.containsKey(requiredSum)) {
+                count += map.get(requiredSum);
+            }
+
+            map.put(currentSum,
+                    map.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        int result = subarraySum(nums, k);
+
+        System.out.println("Number of Subarrays: " + result);
+    }
+}
---
+++ src/Week4/ThreeSum.java
@@ -0,0 +1,63 @@
+package Week4;
+
+import java.util.Arrays;
+
+public class ThreeSum {
+
+    static void threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    System.out.println(
+                            "[" + nums[i] + ", "
+                                    + nums[left] + ", "
+                                    + nums[right] + "]"
+                    );
+
+                    while (left < right &&
+                            nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right &&
+                            nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+
+                    left++;
+
+                } else {
+
+                    right--;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        threeSum(nums);
+    }
+}
